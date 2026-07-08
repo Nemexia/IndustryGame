1181dd85a33bc755c93d531542918581ab4c6466
Nemexia <nemexia.m@gmail.com>
2026-07-09 03:13:18 +0330

    Refactor IRenderer methods to use Position struct for coordinates

--- src/rendering/irenderer.hpp
+++ src/rendering/irenderer.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include "../core/color.hpp"
+#include "../core/position.hpp"
 namespace industry_game
 {
 class IRenderer
 {
   public:
     virtual ~IRenderer() = default;
-    virtual void draw_circle(int x, int y, int radius, Color color, unsigned char alpha) = 0;
-    virtual void draw_rectangle(int x, int y, int width, int height, Color color,
+    virtual void draw_circle(Position position, int radius, Color color, unsigned char alpha) = 0;
+    virtual void draw_rectangle(Position position, int width, int height, Color color,
                                 unsigned char alpha) = 0;
 };
 } // namespace industry_game
