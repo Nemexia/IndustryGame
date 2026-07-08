5c7f269f12cc72aeb8f3b9ca358cbe51e8734244
Nemexia <nemexia.m@gmail.com>
2026-07-09 02:28:23 +0330

    Initialize project structure with CMake configuration and basic game class

---
+++ src/game/game.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace industry_game
+{
+class Game
+{
+  public:
+    void run();
+
+  private:
+    void update();
+    void draw();
+};
+} // namespace industry_game
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "game/game.h"
+
+int main()
+{
+    industry_game::Game game;
+    game.run();
+}
