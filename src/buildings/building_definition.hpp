bd2d74c2d1c8a148a1b41c6c67a95bc943e36912
Nemexia <nemexia.m@gmail.com>
2026-07-09 04:13:42 +0330

    Add BuildingDefinition for coal, iron ore, and steel mines with resource processors

--- src/buildings/building_definition.hpp
+++ src/buildings/building_definition.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#include "../resources/resource_processor.hpp"
+#include "../trucks/truck_definition.hpp"
+
+#include <vector>
 
 namespace industry_game
 {
@@ -7,6 +11,29 @@ struct BuildingDefinition
     const char* name;
     double size;
     double efficiency;
-    // TODO: add trucks
+    std::vector<ResourceProcessor> resource_processors;
+    std::vector<const TruckDefinition*> truck_definitions;
 };
+
+namespace building
+{
+const BuildingDefinition coal_mine = {
+    "Coal Mine",
+    1,
+    0.5,
+    {ResourceProcessor{ResourceStorage{Resource{resource::coal, 0}, 10}, 1}},
+    {&truck::coal_truck}};
+const BuildingDefinition iron_ore_mine = {
+    "Iron Ore Mine",
+    1,
+    0.5,
+    {ResourceProcessor{ResourceStorage{Resource{resource::iron_ore, 0}, 10}, 1}},
+    {&truck::iron_ore_truck}};
+const BuildingDefinition steel_mine = {
+    "Steel Mine",
+    1,
+    0.5,
+    {ResourceProcessor{ResourceStorage{Resource{resource::steel, 0}, 10}, 1}},
+    {&truck::steel_truck}};
+} // namespace building
 } // namespace industry_game
