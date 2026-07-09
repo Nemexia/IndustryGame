003ea6708736b52447e4b72a2a65551afe877a75
Nemexia <nemexia.m@gmail.com>
2026-07-09 04:46:28 +0330

    Add building.hpp header file and include in CMakeLists.txt

---
+++ src/buildings/building.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "../core/position.hpp"
+#include "building_definition.hpp"
+
+#include <vector>
+
+namespace industry_game
+{
+class Building
+{
+  public:
+    Building(const BuildingDefinition& building_definition, Position position);
+
+  private:
+    const BuildingDefinition& building_definiton_;
+    Position position_;
+    std::vector<ResourceProcessor> resource_processors_;
+    std::vector<Truck> trucks_;
+}
+} // namespace industry_game
