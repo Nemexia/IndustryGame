0f92948c7bb98f259c1c0ddae3df6226d258980f
Nemexia <nemexia.m@gmail.com>
2026-07-09 03:46:21 +0330

    Add ResourceDefinition struct for resource properties

--- src/resources/resource_definition.hpp
+++ src/resources/resource_definition.hpp
@@ -7,4 +7,10 @@ struct ResourceDefinition
     const char* name;
     Color color;
 };
+namespace resource
+{
+constexpr ResourceDefinition coal = {"Coal", Color{255, 255, 255}};
+constexpr ResourceDefinition iron_ore = {"Iron Ore", Color{175, 91, 70}};
+constexpr ResourceDefinition steel = {"Steel", Color{122, 127, 128}};
+} // namespace resource
 } // namespace industry_game
