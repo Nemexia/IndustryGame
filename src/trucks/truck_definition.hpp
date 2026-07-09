6d45702abfb1cdc77e51315024a5cf48e4fd2564
Nemexia <nemexia.m@gmail.com>
2026-07-09 03:54:05 +0330

    Add TruckDefinition instances for coal, iron ore, and steel trucks

--- src/trucks/truck_definition.hpp
+++ src/trucks/truck_definition.hpp
@@ -10,4 +10,13 @@ struct TruckDefinition
     double speed;
     ResourceStorage storage;
 };
+namespace truck
+{
+constexpr TruckDefinition coal_truck = {"Coal Truck", 1, 1,
+                                        ResourceStorage{{resource::coal, 0}, 10}};
+constexpr TruckDefinition iron_ore_truck = {"Iron Ore Truck", 1, 1,
+                                            ResourceStorage{{resource::iron_ore, 0}, 10}};
+constexpr TruckDefinition steel_truck = {"Steel Truck", 1, 1,
+                                         ResourceStorage{{resource::steel, 0}, 10}};
+} // namespace truck
 } // namespace industry_game
