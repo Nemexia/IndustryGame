e7c043bb2edf3fc10b1e5783e17026a3eaac905a
Nemexia <nemexia.m@gmail.com>
2026-07-09 03:31:34 +0330

    Add BuildingDefinition struct to manage building properties

---
+++ src/buildings/building_definition.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace industry_game
+{
+struct BuildingDefinition
+{
+    const char* name;
+    double size;
+    double efficiency;
+    // TODO: add trucks
+};
+} // namespace industry_game
