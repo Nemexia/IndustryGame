5e141ba93e3961477ef9ff78eac66f8ed231f0e3
Nemexia <nemexia.m@gmail.com>
2026-07-10 01:15:44 +0330

    Refactor building drawing logic to center position and improve code clarity

--- src/rendering/world_renderer.cpp
+++ src/rendering/world_renderer.cpp
@@ -1,5 +1,7 @@
 #include "world_renderer.hpp"
 
+#include "core/position.hpp"
+
 namespace industry_game
 {
 void WorldRenderer::draw(const World& world, IRenderer& renderer) const
@@ -15,7 +17,9 @@ void WorldRenderer::draw(const World& world, IRenderer& renderer) const
 }
 void WorldRenderer::draw(const Building& building, IRenderer& renderer) const
 {
-    renderer.draw_rectangle(building.get_position(), building.get_size(), building.get_size(),
+    const Position position{building.get_position().x - building.get_size() / 2,
+                            building.get_position().y - building.get_size() / 2};
+    renderer.draw_rectangle(position, building.get_size(), building.get_size(),
                             building.get_color(), building.get_alpha());
 }
 void WorldRenderer::draw(const Truck& truck, IRenderer& renderer) const
