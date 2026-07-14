8bb1cb8539a4ddbb043e991024bc819e99071723
Nemexia <nemexia.m@gmail.com>
2026-07-15 01:15:54 +0330

    Add production logic

--- src/buildings/building.hpp
+++ src/buildings/building.hpp
@@ -41,6 +41,31 @@ class Building
         auto const& current = resource_processors_[0].storage.resource.amount;
         return current / capacity * 255;
     }
+    void update()
+    {
+        for (auto& processor : resource_processors_)
+        {
+            auto const delta = processor.rate * get_building_definition(type_).efficiency;
+            if (delta > 0)
+            {
+                if (processor.storage.resource.amount + delta > processor.storage.capacity)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                if (processor.storage.resource.amount + delta < 0)
+                {
+                    return;
+                }
+            }
+        }
+        for (auto& processor : resource_processors_)
+        {
+            processor.update(get_building_definition(type_).efficiency);
+        }
+    }
 
   private:
     Position position_;
--- src/resources/resource_processor.hpp
+++ src/resources/resource_processor.hpp
@@ -7,5 +7,17 @@ struct ResourceProcessor
 {
     ResourceStorage storage;
     double rate;
+    void update(double efficiency)
+    {
+        storage.resource.amount += rate * efficiency;
+        if (storage.resource.amount > storage.capacity)
+        {
+            storage.resource.amount = storage.capacity;
+        }
+        else if (storage.resource.amount < 0)
+        {
+            storage.resource.amount = 0;
+        }
+    }
 };
 } // namespace industry_game
